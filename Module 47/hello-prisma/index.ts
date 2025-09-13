a76f683fc86d01f0e273f26b9732ee37a28c86b3
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2025-09-13 22:41:25 +0600

    learning pisma

--- Module 47/hello-prisma/index.ts
+++ Module 47/hello-prisma/index.ts
@@ -1,13 +1,28 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const createUser = async () => {
+  const user = await prisma.user.create({
+    data: {
+      email: "1@1.com",
+      name: "John Doe",
+      profilePhoto: "https://example.com/johndoe.jpg",
+    },
+  });
+  console.log(user);
+};
+
 async function main() {
-  console.log("Hello world");
+  // createUser();
+
+  const result = await prisma.user.findMany();
+  console.log(result);
 }
 
 main()
   .then(async () => {
+    console.log("Done");
     await prisma.$disconnect();
   })
   .catch(async (e) => {
---
+++ Module 47/hello-prisma/prisma/migrations/20250913155343_users/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "profilePhoto" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
