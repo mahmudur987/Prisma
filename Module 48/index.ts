b4eb6c4e13eb03660484cd24c9fe0dd0d00c991b
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2025-09-14 20:34:04 +0600

    projexctb initialization done

--- Module 48/index.ts
+++ Module 48/index.ts
@@ -1,2 +1,21 @@
-console.log("Hello World");
-console.log("Hello World");
+import { PrismaClient } from "./generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // createUser();
+
+  const result = await prisma.dentist.findMany();
+  console.log(result);
+}
+
+main()
+  .then(async () => {
+    console.log("Done");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
