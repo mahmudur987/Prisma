987ae2eef5a7fb173f2216dc2cac2ef741ed806c
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2025-09-14 19:55:06 +0600

    complete

--- Module 47/hello-prisma/index.ts
+++ Module 47/hello-prisma/index.ts
@@ -12,7 +12,26 @@ const createUser = async () => {
   });
   console.log(user);
 };
-
+const updateUser = async () => {
+  const user = await prisma.user.update({
+    where: {
+      email: "1@1.com",
+    },
+    data: {
+      name: "John Doe",
+      profilePhoto: "https://example.com/johndoe.jpg",
+    },
+  });
+  console.log(user);
+};
+const deleteUser = async () => {
+  const user = await prisma.user.delete({
+    where: {
+      id: 1,
+    },
+  });
+  console.log(user);
+};
 async function main() {
   // createUser();
 
